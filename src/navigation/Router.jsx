47d29b21ed70a99e99750b5500ab48ad798b31aa
Thiniraxz <thiniragenuka@gmail.com>
2022-09-23 00:18:21 +0530

    Update router

--- src/navigation/Router.jsx
+++ src/navigation/Router.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Candle from "../views/candle/Candle";
+import HomeView from "../views/homeView/homeView";
 import Login from "../views/login/Login";
 import Register from "../views/register/Register";
 
@@ -11,7 +12,7 @@ function Router() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
 
-        <Route path="/" element={<Candle />} />
+        <Route path="/" element={<HomeView />} />
       </Routes>
     </BrowserRouter>
   );
