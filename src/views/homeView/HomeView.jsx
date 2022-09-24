56216d0a94abca6e37e0a00a24836cac67b8860a
Thiniraxz <thiniragenuka@gmail.com>
2022-09-24 17:24:07 +0530

    Update home view

--- src/views/homeView/HomeView.jsx
+++ src/views/homeView/HomeView.jsx
@@ -3,13 +3,48 @@ import HeaderTwo from "../../components/headers/HeaderTwo";
 import AlertDetails from "./AlertDetails";
 import CryptoSec from "./CryptoSec";
 import HomeSubOne from "./HomeSubOne";
+import HomeSubTwo from "./HomeSubTwo";
 import StockSec from "./StockSec";
+import Slide from "react-reveal/Slide";
+import Footer from "./Footer";
 
 function HomeView() {
   return (
-    <div className="homeView">
+    <div className="homeView ">
       <HeaderTwo />
-      <HomeSubOne />
+      <div
+        id="carouselExampleInterval"
+        class="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-inner">
+          <div class="carousel-item active" data-bs-interval="5000">
+            <HomeSubOne />
+          </div>
+          <div class="carousel-item" data-bs-interval="5000">
+            <HomeSubTwo />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleInterval"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleInterval"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
       <div className="app-description d-flex flex-column">
         <header>Crypto Stock Explorer is a platform where you can</header>
         <div className="app-details">
@@ -22,10 +57,15 @@ function HomeView() {
         </div>
       </div>
       <div className="market-cards">
-        <CryptoSec className="crypto-comp"/>
-        <StockSec className="stock-comp"/>
+        <Slide left duration={1500}>
+          <CryptoSec className="crypto-comp" />
+        </Slide>
+        <Slide right duration={1500}>
+          <StockSec className="stock-comp" />
+        </Slide>
       </div>
-      <AlertDetails/>
+      <AlertDetails />
+      <Footer />
     </div>
   );
 }
