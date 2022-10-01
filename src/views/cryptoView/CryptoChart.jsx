03b2c7058f9b14d7c0f9a97c14ff07b48685eece
Thushalya <weeeratharava@gmail.com>
2022-10-02 02:02:30 +0530

    change to 15s

--- src/views/cryptoView/CryptoChart.jsx
+++ src/views/cryptoView/CryptoChart.jsx
@@ -68,7 +68,7 @@ function CryptoChart({market}) {
           },
         });
 
-        let newCrypto = 'http://127.0.0.1:5000/history/'+ `${market}/15s`
+        let newCrypto = 'http://127.0.0.1:5000/history/'+ `${market}/1m`
 
       fetch(newCrypto)
         .then(res => res.json())
@@ -115,7 +115,7 @@ function CryptoChart({market}) {
         console.log('print')
         market='SOL'
         let eventSource = new EventSource(
-          'http://127.0.0.1:5000/present/' + `${market}/`+'15s'
+          'http://127.0.0.1:5000/present/' + `${market}/`+'1m'
         )
         eventSource.addEventListener(
           'message',
