c74b22b363930c8343159eac16327ac300fab6d7
Thiniraxz <thiniragenuka@gmail.com>
2022-10-21 17:42:15 +0530

    Add indicators

--- src/views/cryptoView/CryptoIntervals.jsx
+++ src/views/cryptoView/CryptoIntervals.jsx
@@ -3,17 +3,30 @@ import IndicatorMenuOne from '../../components/indicators/IndicatorMenuOne';
 import IndicatorMenuTwo from '../../components/indicators/IndicatorMenuTwo';
 import { useLocation } from "react-router-dom";
 
-function CryptoIntervals({ changeInterval,timeInterval }) {
+function CryptoIntervals({ changeInterval,timeInterval, addInternalIndicators }) {
   const intervals = ["1m", "5m", "30m", "1h", "1d"];
   var intervalState = location?.state?.interval || "1m";
+
+  // const [indicators, setIndicators] = useState({
+  //   ma: false,
+  //   sma: false,
+  //   ema: false,
+  //   wma: false,
+  //   bbands: false,
+  // });
 
   const handleClick = (interval) => {
     changeInterval(interval);
   };
+
+  const displayInternalIndicators = (indicators) => {
+    addInternalIndicators(indicators);
+  };
+  
   return (
     <div className="CryptoIntervals crypto-bar">
       <div className="d-flex flex-row justify-content-evenly align-items-center">
-        <IndicatorMenuOne />
+        <IndicatorMenuOne displayInternalIndicators={displayInternalIndicators} />
         <IndicatorMenuTwo />
       </div>
       <div className="d-flex flex-row justify content-center">
@@ -23,8 +36,14 @@ function CryptoIntervals({ changeInterval,timeInterval }) {
             return (
               <button
                 type="button"
-                className={" interval-btn "+ interval == (timeInterval || intervalState)? " active-interval":""}
-                onClick={()=>{handleClick(interval)}}
+                className={
+                  " interval-btn " + interval == (timeInterval || intervalState)
+                    ? " active-interval"
+                    : ""
+                }
+                onClick={() => {
+                  handleClick(interval);
+                }}
                 key={interval}
               >
                 {interval}
