d7711198f210f3712a1e3c516289e0b4fc967bd9
Thiniraxz <thiniragenuka@gmail.com>
2022-11-03 14:15:33 +0530

    Make stock chart responsive, initialize indicators

--- src/views/stockView/StockChart.jsx
+++ src/views/stockView/StockChart.jsx
@@ -1,129 +1,154 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { createChart, CrosshairMode } from 'lightweight-charts'
-import Loader from "../../components/loader/Loader"
-import { useLocation } from 'react-router'
-import { compare } from '../../utils/functions'
+import React, { useEffect, useRef, useState } from "react";
+import { createChart, CrosshairMode } from "lightweight-charts";
+import Loader from "../../components/loader/Loader";
+import { useLocation } from "react-router";
+import { compare } from "../../utils/functions";
 import { removeDuplicates } from "../../utils/functions";
 
-function StockChart ({ market, interval }) {
+function StockChart({ market, interval, internalIndicators }) {
+  const location = useLocation();
 
-  const location = useLocation()
-
-  try{
+  try {
     var marketState = location.state.market;
-  }catch(error){
-    marketState="TSLA";
+  } catch (error) {
+    marketState = "TSLA";
   }
-    var intervalState = location?.state?.interval || "5m";
-  
-  const ref = React.useRef()
-  const [loading, setLoading] = useState(false)
+  var intervalState = location?.state?.interval || "5m";
 
-  const [chartData, setChartData] = useState([])
+  const ref = React.useRef();
+  const [loading, setLoading] = useState(false);
 
-  const chart = useRef()
-  const candleSeries = useRef()
+  const [chartData, setChartData] = useState([]);
 
+  const chart = useRef();
+  const candleSeries = useRef();
 
-
-  useEffect(() => {
-      setLoading(true)
-      chart.current = createChart(ref.current, {
-        width: 0,
-        height: 0,
-        layout: {
-          backgroundColor: "#393C45",
-          textColor: "rgba(255, 255, 255, 0.9)",
-        },
-        grid: {
-          vertLines: {
-            color: "rgba(197, 203, 206, 0.5)",
-          },
-          horzLines: {
-            color: "rgba(197, 203, 206, 0.5)",
-          },
-        },
-        crosshair: {
-          mode: CrosshairMode.Normal,
-        },
-
-      });
-      candleSeries.current = chart.current.addCandlestickSeries({
-        upColor: "rgba(0,133,48,1)",
-        downColor: "rgba(162,0,0,1)",
-        borderDownColor: "rgba(162,0,0,1)",
-        borderUpColor: "rgba(0,133,48,1)",
-        wickDownColor: "rgba(162,0,0,1)",
-        wickUpColor: "rgba(0,133,48,1)",
-      });
-      
-      chart.current.applyOptions({
-        timeScale: {
-          visible: true,
-          timeVisible: true,
-           secondsVisible: false
-        }
-      })
-      
-
-      fetch("http://127.0.0.1:5000" + `/stockhistory/${market || marketState}/${interval || intervalState}`)
-        .then((res) => res.json())
-        .then((data) => {
-          let fetchedData = [];
-          // let tempTimeLine = [];
-          data.forEach((row) => {
-            let object = {
-              time: row[0]/1000 ,
-              open: row[1],
-              high: row[2],
-              low: row[3],
-              close: row[4],
-            };
-            fetchedData.push(object);
-          });
-          let tempChartData = removeDuplicates(fetchedData
-          ).sort(compare);
-
-          candleSeries.current.setData(fetchedData);
-          setChartData(tempChartData);
-          setLoading(false);
-          chart.current.resize(1000, 380);
-
-        })
-          .catch();
-
-      return () => {
-        chart.current.remove()   
-      }
-  }, [market,interval])
+  const { ma, sma, ema, wma, bbands } = internalIndicators;
 
   function getWindowDimension() {
     const { innerWidth: width, innerHeight: height } = window;
     return { width, height };
   }
 
-  // const [windowDimensions, setWindowDimensions] = useState(
-  //   getWindowDimension()
-  // );
+  useEffect(() => {
+    setLoading(true);
+    chart.current = createChart(ref.current, {
+      width: 0,
+      height: 0,
+      layout: {
+        backgroundColor: "#393C45",
+        textColor: "rgba(255, 255, 255, 0.9)",
+      },
+      grid: {
+        vertLines: {
+          color: "rgba(197, 203, 206, 0.5)",
+        },
+        horzLines: {
+          color: "rgba(197, 203, 206, 0.5)",
+        },
+      },
+      crosshair: {
+        mode: CrosshairMode.Normal,
+      },
+    });
+    candleSeries.current = chart.current.addCandlestickSeries({
+      upColor: "rgba(0,133,48,1)",
+      downColor: "rgba(162,0,0,1)",
+      borderDownColor: "rgba(162,0,0,1)",
+      borderUpColor: "rgba(0,133,48,1)",
+      wickDownColor: "rgba(162,0,0,1)",
+      wickUpColor: "rgba(0,133,48,1)",
+    });
 
-  // useEffect(() => {
-  //   function handleResize() {
-  //     setWindowDimensions(getWindowDimension());
-  //   }
+    chart.current.applyOptions({
+      timeScale: {
+        visible: true,
+        timeVisible: true,
+        secondsVisible: false,
+      },
+    });
 
-  //   window.addEventListener("resize", handleResize);
-  //   return () => {
-  //     window.removeEventListener("resize", handleResize);
-  //     chart.current.resize(windowDimensions["width"] * 0.85, 380);
-  //   };
-  // });
+    fetch(
+      "http://127.0.0.1:5000" +
+        `/stockhistory/${market || marketState}/${interval || intervalState}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        let fetchedData = [];
+        // let tempTimeLine = [];
+        data.forEach((row) => {
+          let object = {
+            time: row[0] / 1000,
+            open: row[1],
+            high: row[2],
+            low: row[3],
+            close: row[4],
+          };
+          fetchedData.push(object);
+        });
+        let tempChartData = removeDuplicates(fetchedData).sort(compare);
+
+        candleSeries.current.setData(fetchedData);
+        setChartData(tempChartData);
+        setLoading(false);
+        chart.current.resize(1000, 380);
+      })
+      .catch();
+
+    if (ma) {
+      console.log("Ma true");
+    }
+
+    return () => {
+      chart.current.remove();
+    };
+  }, [market, interval, internalIndicators]);
+
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimension()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimension());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      // chart.current.resize(windowDimensions["width"] * 0.85, 380);
+      const width = windowDimensions["width"];
+      if (width >= 1220) {
+        chart.current.resize(1067, 380);
+      }
+      if (width >= 1070 && width < 1220) {
+        chart.current.resize(930, 380);
+      }
+      if (width >= 900 && width < 1070) {
+        chart.current.resize(800, 380);
+      }
+      if (width >= 800 && width < 900) {
+        chart.current.resize(670, 380);
+      }
+      if (width >= 650 && width < 800) {
+        chart.current.resize(540, 380);
+      }
+      if (width >= 550 && width < 650) chart.current.resize(430, 340);
+      if (width >= 478 && width < 550) {
+        chart.current.resize(380, 320);
+      }
+      if (width > 350 && width < 478) chart.current.resize(320, 280);
+    };
+  });
+
+
 
   return (
     <>
-      {loading ? <Loader position="relative" left="46.5%" top="9%"/> : null}
-      <div className='StockChart' ref={ref}  />
+      {loading ? <Loader position="relative" left="46.5%" top="9%" /> : null}
+      <div className="StockChart" ref={ref} />
     </>
-  )
+  );
 }
 
-export default StockChart
+export default StockChart;
--- src/views/stockView/StockView.jsx
+++ src/views/stockView/StockView.jsx
@@ -9,12 +9,23 @@ function StockView() {
 
   const [market, setMarket] = useState("");
   const [interval,setInterval] = useState("")
+  const [internalIndicators, setInternalIndicators] = useState({
+    ma: false,
+    sma: false,
+    ema: false,
+    wma: false,
+    bbands: false,
+  });
+
   const changeStockType = (marketType) => {
     setMarket(marketType);
   };
   const changeInterval = (interval)=>{
     setInterval(interval)
   }
+  const addInternalIndicators = (indicators) => {
+    setInternalIndicators(indicators);
+  };
 
   return (
     <div className="CryptoView">
@@ -22,8 +33,8 @@ function StockView() {
       <div className="d-flex flex-row">
         <div className="crypto-charts d-flex flex-column">
           <StockHeader market={market} interval={interval} />
-          <StockIntervals changeInterval={changeInterval} />
-          <StockChart market={market} interval={interval} />
+          <StockIntervals changeInterval={changeInterval} addInternalIndicators={addInternalIndicators}/>
+          <StockChart market={market} interval={interval} internalIndicators={internalIndicators}/>
         </div>
         <div className="types-crypto">
           <StockTypes changeStockType={changeStockType} />
