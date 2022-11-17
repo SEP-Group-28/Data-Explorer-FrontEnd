84e4b375407b8e96d59a35b1b4a50228746ff8c5
AwesomeNipun <nipun1969@gmail.com>
2022-11-17 09:55:13 +0530

    add e2e testing support

--- cypress.config.js
+++ cypress.config.js
@@ -14,4 +14,11 @@ export default defineConfig({
       bundler: "webpack",
     },
   },
+
+  e2e: {
+    baseUrl: "http://localhost:5173",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 });
